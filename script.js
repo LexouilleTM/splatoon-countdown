aff753b56d68abb84f4a55d01be9f536b14d83ce
LéxouilleTM <92437828+LexouilleTM@users.noreply.github.com>
2025-03-16 17:41:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function updateCountdown() {
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
 
-    countdownElement.textContent = `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    countdownElement.textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
 
 function startConfetti() {
